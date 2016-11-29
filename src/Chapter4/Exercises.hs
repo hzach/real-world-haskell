590f7241dd1834b68745268fdc8f0223616e0c0f
hzach <zftidwell@gmail.com>
2016-11-28 21:36:34 -0600

    implement asInt function with folding; add a test

--- src/Chapter4/Exercises.hs
+++ src/Chapter4/Exercises.hs
@@ -1,5 +1,9 @@
 -- file: ch04/ch04.exercises.hs
-module Chapter4.Exercises where
+module Chapter4.Exercises
+       (splitWith
+      , asIntFold
+        ) where
+
   import Data.Char
   import Chapter4.SafeList
 
@@ -11,5 +15,19 @@ module Chapter4.Exercises where
               Just xs -> splitWith p xs
               Nothing -> []
 
-  asInt_fold :: String -> Int
-  asInt_fold _ = error "not implemented"
+
+  -- Converts a given numerical String an Int.
+  -- Requires that the String contain at least one
+  -- digit and no non-numerical characters except
+  -- for an optional sign.
+  asIntFold :: String -> Int
+  asIntFold "" = error "Empty String"
+  asIntFold s | head s == '-' = combine (-) $ toIntList $ tail s
+              | otherwise     = combine (+) $ toIntList s
+      where
+        toIntList [] = error "Non-numerical string"
+        toIntList xs = map toInt xs
+        toInt x | isDigit x = digitToInt x
+                | otherwise = error "Non-numerical string"
+        combine op = foldl (\x y -> op (10*x) y)  0
+  ------------------------------------------------------------------------------
