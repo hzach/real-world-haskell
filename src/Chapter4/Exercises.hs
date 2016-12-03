1bf47a18add905ecbbfea5012834ecf7ea26614c
hzach <zftidwell@gmail.com>
2016-12-02 20:40:14 -0600

    add asIntEither with a test

--- src/Chapter4/Exercises.hs
+++ src/Chapter4/Exercises.hs
@@ -2,6 +2,7 @@
 module Chapter4.Exercises
        (splitWith
       , asIntFold
+      , asIntEither
         ) where
 
   import Data.Char
@@ -16,9 +17,9 @@ module Chapter4.Exercises
               Nothing -> []
 
 
-  -- Converts a given numerical String an Int.
-  -- Requires that the String contain at least one
+  -- Converts a given numerical String to an Int.
   -- digit and no non-numerical characters except
+  -- Requires that the String contain at least one
   -- for an optional sign.
   asIntFold :: String -> Int
   asIntFold "" = error "Empty String"
@@ -29,5 +30,23 @@ module Chapter4.Exercises
         toIntList xs = map toInt xs
         toInt x | isDigit x = digitToInt x
                 | otherwise = error "Non-numerical string"
-        combine op = foldl (\x y -> op (10*x) y)  0
-  ------------------------------------------------------------------------------
+        combine op = foldl (\x y -> op (10*x) y) 0
+--------------------------------------------------------------------------------
+
+  type ErrorMessage = String
+
+  -- Converts a given numerical String to an Int.
+  -- digit and no non-numerical characters except
+  -- Requires that the String contain at least one
+  -- for an optional sign.
+  asIntEither :: String -> Either ErrorMessage Int
+  asIntEither ""       = Left "Empty String"
+  asIntEither ['-']    = Left "Invalid"
+  asIntEither ('-':bs) = foldl (combine (-)) (Right 0) bs
+  asIntEither cs       = foldl (combine (+)) (Right 0) cs
+
+  combine op (Right x) c | isDigit c    = Right $ op (10*x) (digitToInt c)
+                         | maxBound < x = Left "Integer Overflow"
+                         | otherwise    = Left $ "Not a digit '" ++ [c] ++ "'"
+  combine _ msg@(Left s) _              = msg
+--------------------------------------------------------------------------------
--- test/Chapter4/ExercisesSpec.hs
+++ test/Chapter4/ExercisesSpec.hs
@@ -8,16 +8,27 @@ module Chapter4.ExercisesSpec where
   import Control.Exception
 
   spec :: Spec
-  spec =
+  spec = do
     describe "asIntFold" $ do
-
       context "with good input" $
         it "returns an Int" $ do
           asIntFold "1337" `shouldBe` 1337
           asIntFold "-420" `shouldBe` -420
-
       context "with bad input" $
         it "throws an exception" $ do
-          evaluate (asIntFold "") `shouldThrow` anyErrorCall
-          evaluate (asIntFold "-") `shouldThrow` anyErrorCall
+          evaluate (asIntFold "")                `shouldThrow` anyErrorCall
+          evaluate (asIntFold "-")               `shouldThrow` anyErrorCall
           evaluate (asIntFold "4lph<a>Num3ric!") `shouldThrow` anyErrorCall
+
+    describe "asIntEither" $ do
+      context "with good input" $
+        it "returns an Int" $ do
+          asIntEither "0"    `shouldBe` Right 0
+          asIntEither "1"    `shouldBe` Right 1
+          asIntEither "1337" `shouldBe` Right 1337
+          asIntEither "-420" `shouldBe` Right (-420)
+      context "with bad input" $
+        it "returns an ErrorMessage" $ do
+          asIntEither ""  `shouldBe` Left "Empty String"
+          asIntEither "-" `shouldBe` Left "Invalid"
+          asIntEither "4o4" `shouldBe` Left "Not a digit 'o'"
